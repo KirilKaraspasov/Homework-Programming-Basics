668366bd6e9131719bbc77e01f02150c024e4779
KirilKaraspasov <47929445+KirilKaraspasov@users.noreply.github.com>
2022-05-13 22:21:20 +0300

    Create RectangleArea.java

---
+++ Programming Basics/src/RectangleArea.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            int side = Integer.parseInt(scanner.nextLine());
+            int area = side*side;
+            System.out.println(area);
+            
+        }
+}
